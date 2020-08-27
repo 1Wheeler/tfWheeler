dc898d154dbabe5f6a02ab8900d774e8da0dd65a
Jeff Wheeler <jeff@1wheeler.com>
2020-08-27 00:04:12 -0600

    Update vmware.tf

--- vmware.tf
+++ vmware.tf
@@ -6,8 +6,6 @@ provider "vsphere" {
   user           = "${var.cloudUsername}"
   password       = "${var.cloudPassword}"
   vsphere_server = "10.30.21.180"
-
-  # if you have a self-signed cert
   allow_unverified_ssl = true
 }
 
@@ -39,31 +37,25 @@ resource "vsphere_virtual_machine" "vm" {
   name             = "${var.instanceName}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
   num_cpus = 1
   memory   = 1024
   guest_id = "ubuntu64Guest"
-
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
-
   disk {
     label            = "disk0"
     thin_provisioned = true
     size             = 15
   }
-
   disk {
     label       = "disk1"
     size        = 5
     unit_number = 1
   }
-
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
   }
-
   connection {
     type     = "ssh"
     user     = "cloud-user"
